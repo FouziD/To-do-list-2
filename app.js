2cbada3a62397e04d7c8d5b3f2317d20a7d986dd
FouziD <144001834+FouziD@users.noreply.github.com>
2024-01-10 16:03:47 +0100

    i continue d working on the project but i got another issue and that is that i can't add any text in the app

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const todoList = document.getElementById('todo-list');
 
 //Add a task
 const addTask =() => {
-const taskText = todoInput.ariaValueMax.trim();
+const taskText = todoInput.value.trim();
 
 if(taskText !==''){
     const taskItem = createTaskItem(taskText)
     todoList.appendChild(taskItem)
     todoInput.value = '';
 }
-}
+};
 //Create new task items
 const createTaskItem = (taskText) => {
     const taskItem = document.createElement('li');
@@ -26,27 +26,36 @@ const createTaskItem = (taskText) => {
 
     const deleteBtn = document.createElement('button');
     deleteBtn.textContent = 'Delete'
-    document.classList.add('delete-btn')
-    deleteBtn.addEventListener('click', deleteTask)
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', deleteTask);
 
-    taskItem.appendChild(checkbox)
-    taskItem.appendChild(taskTextSpan)
-    taskItem.appendChild(deleteBtn)
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTextSpan);
+    taskItem.appendChild(deleteBtn);
 
     return taskItem;
 };
 // Delete out tasks
-const deleteTask = () => 
+const deleteTask = (event) => 
 {
     const taskItem = event.target.parentNode;
     todoList.removeChild(taskItem);
 };
 // cross out tasks
-const toggleTask = () =>
+const toggleTask = (event) =>
 {
 const taskItem = event.target.parentNode;
-taskItem.classList.toggle('completed')
+taskItem.classList.toggle('completed');
 };
 //Event Listeners
+addTaskButton.addEventListener('click', addTask)
+todoInput.addEventListener('keydown', function (event)
+{
+    if(event.key == 'Enter'){
+        addTask();
+    }
+})
 
+todoList.addEventListener('change', toggleTask);
 //Examples of task
+
